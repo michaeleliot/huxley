121970f11fc374ccd2e4cc82d402e10561d1a521
michaeleliot <michael.eliot@aol.com>
2016-12-07 11:38:58 -0800

    missed a merge

--- huxley/core/migrations/0017_auto_20161202_1423.py
+++ huxley/core/migrations/0017_auto_20161202_1423.py
@@ -6,19 +6,12 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-<<<<<<< HEAD
-    dependencies = [
-        ('core', '0016_auto_20160724_2344'),
-    ]
-=======
     dependencies = [('core', '0016_auto_20160724_2344'), ]
->>>>>>> 51e3a3a85781b47f6dc44f6bec10b7a87a589095
 
     operations = [
         migrations.AddField(
             model_name='delegate',
             name='friday_attendance',
-<<<<<<< HEAD
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
@@ -36,19 +29,4 @@ class Migration(migrations.Migration):
             name='sunday_attendance',
             field=models.BooleanField(default=False),
         ),
-=======
-            field=models.BooleanField(default=False), ),
-        migrations.AddField(
-            model_name='delegate',
-            name='saturday_afternoon_attendance',
-            field=models.BooleanField(default=False), ),
-        migrations.AddField(
-            model_name='delegate',
-            name='saturday_morning_attendance',
-            field=models.BooleanField(default=False), ),
-        migrations.AddField(
-            model_name='delegate',
-            name='sunday_attendance',
-            field=models.BooleanField(default=False), ),
->>>>>>> 51e3a3a85781b47f6dc44f6bec10b7a87a589095
     ]
